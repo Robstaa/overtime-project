d72fd39db3d951bfe556462485847ac8cfba9bda
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-13 18:13:44 +0100

    Added show, create and new method in Post controller and created the belonging views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(give_post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  private
+
+  def give_post_params
+    params.require(:post).permit(:date, :rationale)
+  end
 end
