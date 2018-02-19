4996d0cbf93cd63ba9ecbbec54033d0a8ff28192
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-19 16:13:03 +0100

    Implemented admin dashboard with administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :posts
+    resources :users
+    resources :admin_users
+
+    root to: "posts#index"
+  end
+
   devise_for :users, skip: [:registrations]
   resources :posts
   root to: 'static#homepage'
