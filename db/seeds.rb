fc989034cd9f2160902c1ec10640b3fdcccef3b1
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-20 21:16:41 +0100

    Admin dashboard link in navbar for admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 require 'faker'
 
-puts "Creating 100 Posts"
+puts "Creates one Admin and one regular User"
 
 user1 = User.create(email: "test@test.com", password: "123456", first_name: "Robstaa", last_name: "Sushi")
+admin = AdminUser.create(email: "admin@test.com", password: "123456", first_name: "Admin", last_name: "User")
+
+puts "Creating 100 Posts"
 
 100.times do
   Post.create(date: Date.today, rationale: Faker::WorldOfWarcraft.hero, user_id: user1.id)
--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -1,31 +1,51 @@
 require 'rails_helper'
 
 describe 'Admin Dashboard' do
-  it 'can be reached' do
-    visit admin_root_path
-    expect(page.status_code).to eq(200)
+
+  describe 'navigation' do
+    it 'can be reached' do
+      visit admin_root_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'redirects to "Sign in" for users who are not signed in' do
+      visit admin_root_path
+      expect(current_path).to eq(new_user_session_path)
+    end
+
+    it 'cannot be reached by regular users' do
+      user = FactoryBot.create(:user)
+      login_as(user, scope: :user)
+
+      visit admin_root_path
+
+      expect(current_path).not_to eq(admin_root_path)
+    end
+
+    it 'can be reached by admin users' do
+      admin = FactoryBot.create(:admin)
+      login_as(admin, scope: :user)
+
+      visit admin_root_path
+
+      expect(current_path).to eq(admin_root_path)
+    end
   end
 
-  it 'redirects to "Sign in" for users who are not signed in' do
-    visit admin_root_path
-    expect(current_path).to eq(new_user_session_path)
-  end
+  describe 'post status' do
+    before do
+      admin = FactoryBot.create(:admin)
+      login_as(admin, scope: :user)
+      @post = FactoryBot.create(:post)
+    end
 
-  it 'cannot be reached by regular users' do
-    user = FactoryBot.create(:user)
-    login_as(user, scope: :user)
+    it 'can be changed by admin' do
+      visit edit_admin_post_path(@post)
 
-    visit admin_root_path
+      select("approved", from: "post_status", visible: false).select_option
+      click_button("Update Post")
 
-    expect(current_path).not_to eq(admin_root_path)
-  end
-
-  it 'can be reached by admin users' do
-    admin = FactoryBot.create(:admin)
-    login_as(admin, scope: :user)
-
-    visit admin_root_path
-
-    expect(current_path).to eq(admin_root_path)
+      expect(page).to have_content("approved")
+    end
   end
 end
