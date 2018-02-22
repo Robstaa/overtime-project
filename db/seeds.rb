20c607f4ffec687819c75713e146381ec7065e1d
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-22 13:21:57 +0100

    Implemented audit log tab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,16 @@ puts "Creates one Admin and one regular User"
 user1 = User.create(email: "test@test.com", password: "123456", first_name: "Robstaa", last_name: "Sushi")
 admin = AdminUser.create(email: "admin@test.com", password: "123456", first_name: "Admin", last_name: "User")
 
-puts "Creating 100 Posts"
+puts "Creating 100 User Posts"
 
 100.times do
   Post.create(date: Date.today, rationale: Faker::WorldOfWarcraft.hero, user_id: user1.id, overtime_request: 2.5)
+end
+
+puts "Creating 10 Admin Posts"
+
+10.times do
+  Post.create(date: Date.today, rationale: Faker::WorldOfWarcraft.hero, user_id: admin.id, overtime_request: 2.5)
 end
 
 puts "Creates 10 Audit Logs"
