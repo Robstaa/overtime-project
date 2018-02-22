af51cbd0a520ef42a15fc68d6fe70b58e9a79d7b
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-22 10:43:04 +0100

    Updated audit log factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,10 @@ puts "Creating 100 Posts"
 100.times do
   Post.create(date: Date.today, rationale: Faker::WorldOfWarcraft.hero, user_id: user1.id, overtime_request: 2.5)
 end
+
+puts "Creates 10 Audit Logs"
+
+10.times do
+  FactoryBot.create(:audit_log)
+end
+
