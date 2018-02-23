7cd6a8b7d9bcd243d40569a121efaefd61240326
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-23 20:05:36 +0100

    Implemented connection to Twilio to send text messages

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,15 @@
 module SmsTool
-  def self.send_sms(number:, message:)
-    puts "Sending Sms"
-    puts "#{message} to #{number}"
+  account_sid = ENV['TWILIO_ACCOUNT_SID']
+  auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+  @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+  def self.send_sms(number, message)
+    @client.api.account.messages.create(
+      from: "+4915735994341",
+      to: "#{number}",
+      body: "#{message}")
   end
 end
+
+# SmsTool.send_sms("+4917661387174", "ONE MORE TRY ;)")
