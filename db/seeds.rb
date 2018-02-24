9c9fc5f3ad6ee9f63018a2593fcb02647d5689b4
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-24 15:32:33 +0100

    Implemented the audit log summary on homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ puts "Creates one Admin and one regular User"
 
 user1 = User.create(email: "test@test.com",
                     password: "123456",
-                    first_name: "Robstaa",
-                    last_name: "Sushi",
+                    first_name: Faker::Name.first_name,
+                    last_name: Faker::Name.last_name,
                     phone_number: "004917661387174")
 
 admin = AdminUser.create(email: "admin@test.com",
@@ -14,21 +14,21 @@ admin = AdminUser.create(email: "admin@test.com",
                         last_name: "User",
                         phone_number: "004917661387174")
 
-puts "Creating 100 User Posts"
+puts "Creating 10 User Posts"
 
-100.times do
+10.times do
   Post.create(date: Date.today, rationale: Faker::WorldOfWarcraft.hero, user_id: user1.id, overtime_request: 2.5)
 end
 
-puts "Creating 10 Admin Posts"
+puts "Creating 5 Admin Posts"
 
-50.times do
+5.times do
   Post.create(date: Date.today, rationale: Faker::WorldOfWarcraft.hero, user_id: admin.id, overtime_request: 2.5)
 end
 
 puts "Creates 10 Audit Logs"
 
-50.times do
+10.times do
   FactoryBot.create(:audit_log, user_id: user1.id)
 end
 
