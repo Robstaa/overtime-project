4bfdda9647cf44608f834cf7bb4c7935bed0c176
Robin Zuschke <robin.l.zuschke@gmail.com>
2018-02-25 01:14:31 +0100

    Doesnt show audit log for employee when overtime for this week is requested

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,13 @@ class Post < ApplicationRecord
   validates :rationale, presence: true
   validates :overtime_request, presence: true, numericality: {greater_than: 0.0}
   belongs_to :user
+
+  after_save :update_audit_log
+
+  private
+
+    def update_audit_log
+      audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date - 7.days..self.date)).last
+      audit_log.confirmed! if audit_log
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,12 @@ admin = AdminUser.create!(email: "admin@test.com",
                         last_name: "User",
                         phone_number: "004917661387174")
 
+puts "Creates 3 Audit Logs"
+
+AuditLog.create!(start_date: Date.today - 6.days, status: "pending", user_id: user1.id)
+AuditLog.create!(start_date: Date.today - 13.days, status: "pending", user_id: user1.id)
+AuditLog.create!(start_date: Date.today - 20.days, status: "pending", user_id: user1.id)
+
 puts "Creating 10 User Posts"
 
 10.times do
@@ -26,11 +32,6 @@ puts "Creating 5 Admin Posts"
   Post.create(date: Date.today, rationale: Faker::WorldOfWarcraft.hero, user_id: admin.id, overtime_request: 2.5)
 end
 
-puts "Creates 10 Audit Logs"
 
-# FactoryBot.create(:audit_log, user_id: user1.id)
-AuditLog.create!(start_date: Date.today - 6.days, status: "pending", user_id: user1.id)
-AuditLog.create!(start_date: Date.today - 13.days, status: "pending", user_id: user1.id)
-AuditLog.create!(start_date: Date.today - 20.days, status: "pending", user_id: user1.id)
 
 
